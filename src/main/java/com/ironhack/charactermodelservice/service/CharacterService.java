5c8096b8081286de0fef6913a6bc078b1b42fad1
João Afonso Silva <joaoafonsodss@gmail.com>
2021-11-25 23:46:03 +0000

    REfactor return from service

--- src/main/java/com/ironhack/charactermodelservice/service/CharacterService.java
+++ src/main/java/com/ironhack/charactermodelservice/service/CharacterService.java
@@ -11,15 +11,15 @@ public interface CharacterService {
 
     List<CharacterDTO> getAllCharacters();
 
-    Optional<CharacterDTO> getCharacterById(Long id);
+    CharacterDTO getCharacterById(Long id);
 
     List<CharacterDTO> getAliveCharactersByUserUsername(String username);
 
     List<CharacterDTO> getDeadCharactersByUserUsername(String username);
 
-    Optional<CharacterDTO> createCharacter(NewCharacterDTO newCharacterDTO);
+    CharacterDTO createCharacter(NewCharacterDTO newCharacterDTO);
 
-    Optional<CharacterDTO> updateCharacter(UpdateCharacterDTO updateCharacterDTO);
+    CharacterDTO updateCharacter(UpdateCharacterDTO updateCharacterDTO);
 
     void deleteCharacter(Long id);
 
