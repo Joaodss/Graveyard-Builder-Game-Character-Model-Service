f930533e7d2603426cced3e80fa31c4b3c7d61d2
João Afonso Silva <joaoafonsodss@gmail.com>
2021-11-25 16:01:49 +0000

    Add profile for testing spring applications

--- src/test/java/com/ironhack/charactermodelservice/CharacterModelServiceApplicationTests.java
+++ src/test/java/com/ironhack/charactermodelservice/CharacterModelServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.ironhack.charactermodelservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CharacterModelServiceApplicationTests {
 
     @Test
