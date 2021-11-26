b584711882971faba8fd784f6e1e918bcf680ff6
João Afonso Silva <joaoafonsodss@gmail.com>
2021-11-26 11:04:11 +0000

    Add controller implementation

--- src/main/java/com/ironhack/charactermodelservice/controller/CharacterControllerImpl.java
+++ src/main/java/com/ironhack/charactermodelservice/controller/CharacterControllerImpl.java
@@ -2,42 +2,89 @@ package com.ironhack.charactermodelservice.controller;
 
 import com.ironhack.charactermodelservice.dto.CharacterDTO;
 import com.ironhack.charactermodelservice.dto.NewCharacterDTO;
+import com.ironhack.charactermodelservice.service.CharacterService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping("/api/v1/characters")
+@RequiredArgsConstructor
+@Slf4j
 public class CharacterControllerImpl implements CharacterController {
-    @Override
+    private final CharacterService characterService;
+
+
+    // -------------------- GET --------------------
+    @GetMapping("/all")
+    @ResponseStatus(OK)
     public List<CharacterDTO> getAllCharacters() {
-        return null;
+        log.info("Getting all characters");
+        return characterService.getAllCharacters();
     }
 
-    @Override
-    public CharacterDTO getCharacterById(Long id) {
-        return null;
+    @GetMapping("/id/{id}")
+    @ResponseStatus(OK)
+    public CharacterDTO getCharacterById(@PathVariable Long id) {
+        log.info("Getting character by id {}", id);
+        var character = characterService.getCharacterById(id);
+        if (character == null) throw new ResponseStatusException(NOT_FOUND, "Character not found");
+        return character;
     }
 
-    @Override
-    public List<CharacterDTO> getCharactersByUserUsernameActive(String username) {
-        return null;
+    @GetMapping("/party/{username}")
+    @ResponseStatus(OK)
+    public List<CharacterDTO> getCharactersByUserUsernameActive(@PathVariable String username) {
+        log.info("Getting party by user username {}", username);
+        return characterService.getAliveCharactersByUserUsername(username);
     }
 
-    @Override
-    public List<CharacterDTO> getCharactersByUserUsernameGraveyard(String username) {
-        return null;
+    @GetMapping("/graveyard/{username}")
+    @ResponseStatus(OK)
+    public List<CharacterDTO> getCharactersByUserUsernameGraveyard(@PathVariable String username) {
+        log.info("Getting graveyard by user username {}", username);
+        return characterService.getDeadCharactersByUserUsername(username);
     }
 
-    @Override
+
+    // -------------------- POST --------------------
+    @PostMapping("/create")
+    @ResponseStatus(CREATED)
     public CharacterDTO createCharacter(NewCharacterDTO newCharacterDTO) {
-        return null;
+        log.info("Creating character {} of type {}", newCharacterDTO.getName(), newCharacterDTO.getType());
+        return characterService.createCharacter(newCharacterDTO);
     }
 
-    @Override
+
+    // -------------------- PUT --------------------
+    @PutMapping("/update")
+    @ResponseStatus(OK)
     public CharacterDTO updateCharacter(CharacterDTO updateCharacterDTO) {
-        return null;
+        log.info("Updating character {}", updateCharacterDTO.getId());
+        try {
+            return characterService.updateCharacter(updateCharacterDTO);
+        } catch (EntityNotFoundException e1) {
+            throw new ResponseStatusException(BAD_REQUEST, "Character not found");
+        }
     }
 
-    @Override
-    public void deleteCharacterById(Long id) {
 
+    // -------------------- DELETE --------------------
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(OK)
+    public void deleteCharacterById(@PathVariable Long id) {
+        log.info("Deleting character {}", id);
+        try {
+            characterService.deleteCharacter(id);
+        } catch (EntityNotFoundException e1) {
+            throw new ResponseStatusException(BAD_REQUEST, "Character not found");
+        }
     }
+
 }
